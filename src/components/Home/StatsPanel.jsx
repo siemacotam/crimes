97cbd622f4eb03296e7f60af3403c30545693075
Grzegorz Celek <g.celek@gmail.com>
2021-11-02 15:20:11 +0100

    small changes

--- src/components/Home/StatsPanel.jsx
+++ src/components/Home/StatsPanel.jsx
@@ -1,7 +1,7 @@
 const StatsPanel = () => {
   return (
     <div className="home__statsPanel">
-      <p>stats</p>
+      <p className="text-bold">Statystyki</p>
       <p>1000 graczy</p>
       <p>aktywni : 100</p>
     </div>
--- src/components/Home/TopPlayers.jsx
+++ src/components/Home/TopPlayers.jsx
@@ -1,8 +1,31 @@
 const TopPlayers = () => {
   return (
     <div className="home__topPlayers">
-      <p>top gracze</p>
-      tabelka
+      <p className="text-bold">Top gracze</p>
+      <table>
+        <thead>
+          <tr>
+            <th>M-ce</th>
+            <th>Uzytkownik</th>
+            <th>Respekt</th>
+          </tr>
+        </thead>
+        <tr>
+          <td>1</td>
+          <td>Maria</td>
+          <td>100</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Francisco</td>
+          <td>80</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Jacob</td>
+          <td>60</td>
+        </tr>
+      </table>
     </div>
   );
 };
