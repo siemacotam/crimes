b99e32314a008a3d79236edade00ac87afaa8714
Grzegorz Celek <g.celek@gmail.com>
2021-11-03 10:11:23 +0100

    create footer component

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer__container-up">siema</div>
+      <div className="footer__container-down">
+        <p className="text-center text-white">
+          Prawa zastrzeżone @ MainShop 2021
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
